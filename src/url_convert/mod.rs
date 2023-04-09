5e7ab64263f4795f5882526a4f044c3b157ea1c7
Mattia Di Eleuterio <madiele92@gmail.com>
2023-04-09 19:27:40 +0200

    the end

--- src/url_convert/mod.rs
+++ src/url_convert/mod.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 use reqwest::Url;
+use tokio::process::Command;
+use cached::{proc_macro::io_cached, AsyncRedisCache};
 
 pub fn from(url: Url) -> Box<dyn ConvertableToFeed> {
     let url = url.to_owned();
@@ -12,7 +14,7 @@ pub fn from(url: Url) -> Box<dyn ConvertableToFeed> {
 
 #[async_trait]
 pub trait ConvertableToFeed {
-    async fn to_feed_url(&self) -> Url;
+    async fn to_feed_url(&self) -> eyre::Result<Url>;
     fn is(&self) -> &str; //usefull for testing
 }
 
@@ -22,8 +24,12 @@ struct TwitchUrl {
 
 #[async_trait]
 impl ConvertableToFeed for TwitchUrl {
-    async fn to_feed_url(&self) -> Url {
-        self.url.to_owned()
+    async fn to_feed_url(&self) -> eyre::Result<Url> {
+        let username = self.url.path_segments().ok_or_else(|| eyre::eyre!("Unable to get path segments"))?.last().ok_or_else(|| eyre::eyre!("Unable to get last path segment"))?;
+        let mut url = Url::parse(&format!("{}{}", std::env::var("TWITCH_TO_PODCAST_URL")?, "/vod/")).map_err(|err| eyre::eyre!(err))?;
+        url.path_segments_mut().map_err(|_| eyre::eyre!("Unable to get mutable path segments"))?.push(username);
+        url.query_pairs_mut().append_pair("transcode", "false");
+        Ok(url)
     }
 
     fn is(&self) -> &str { "TwitchUrl" }
@@ -34,10 +40,43 @@ struct YoutubeUrl {
     url: Url,
 }
 
+#[cfg_attr(not(test),
+io_cached(
+    map_error = r##"|e| eyre::Error::new(e)"##,
+    type = "AsyncRedisCache<Url, Url>",
+    create = r##" {
+        let redis_address = std::env::var("REDIS_ADDRESS").unwrap_or_else(|_| "localhost".to_string());
+        let redis_port = std::env::var("REDIS_PORT").unwrap_or_else(|_| "6379".to_string());
+
+        AsyncRedisCache::new("youtube_channel_username_to_id=", 9999999)
+            .set_refresh(false)
+            .set_connection_string(&format!("redis://{}:{}/", redis_address, redis_port))
+            .build()
+            .await
+            .expect("youtube_channel_username_to_id cache")
+} "##
+))]
+async fn find_yt_channel_url_with_c_id(url: &Url) -> eyre::Result<Url> {
+    let output = Command::new("yt-dlp")
+        .arg("--playlist-items")
+        .arg("0")
+        .arg("-O")
+        .arg("playlist:channel_url")
+        .arg(url.to_string())
+        .output()
+    .await?;
+    let feed_url = std::str::from_utf8(&output.stdout)?.trim().to_string();
+    Ok(Url::parse(&feed_url)?)
+}
+
 #[async_trait]
 impl ConvertableToFeed for YoutubeUrl {
-    async fn to_feed_url(&self) -> Url {
-        self.url.to_owned()
+    async fn to_feed_url(&self) -> eyre::Result<Url> {
+        let url_with_channel_id = find_yt_channel_url_with_c_id(&self.url).await?;
+        let channel_id = url_with_channel_id.path_segments().unwrap().last().unwrap();
+        let mut feed_url = Url::parse("https://www.youtube.com/feeds/videos.xml")?;
+        feed_url.query_pairs_mut().append_pair("channel_id", channel_id);
+        Ok(feed_url)
     }
     fn is(&self) -> &str { "YoutubeUrl" }
 }
@@ -48,8 +87,8 @@ struct GenericUrl {
 
 #[async_trait]
 impl ConvertableToFeed for GenericUrl {
-    async fn to_feed_url(&self) -> Url {
-        self.url.to_owned()
+    async fn to_feed_url(&self) -> eyre::Result<Url> {
+        Ok(self.url.to_owned())
     }
 
     fn is(&self) -> &str { "GenericUrl" }
@@ -75,6 +114,14 @@ mod tests {
         let url = Url::parse("https://youtu.be/dQw4w9WgXcQ").unwrap();
         let convertable = from(url);
         assert!(convertable.is() == "YoutubeUrl");
+    }
+
+    #[tokio::test]
+    async fn test_youtube_to_feed() {
+        let url = "https://www.youtube.com/channel/UC-lHJZR3Gqxm24_Vd_AJ5Yw";
+        let channel = YoutubeUrl { url: Url::parse(url).unwrap() };
+        let feed_url = channel.to_feed_url().await.unwrap();
+        assert_eq!(feed_url.as_str(), "https://www.youtube.com/feeds/videos.xml?channel_id=UC-lHJZR3Gqxm24_Vd_AJ5Yw");
     }
 
     #[test]
