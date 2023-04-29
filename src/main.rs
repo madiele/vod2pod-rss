fe821a268461f1dc140bbb510754078670f83751
Mattia Di Eleuterio <madiele92@gmail.com>
2023-04-29 12:47:24 +0200

    test hash

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use vod2pod_rss::{
     transcoder::{ Transcoder, FfmpegParameters, FFMPEGAudioCodec }, rss_transcodizer::RssTranscodizer, url_convert, configs::{Conf, conf, ConfName},
 };
 
-//cache test
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     SimpleLogger::new().with_level(log::LevelFilter::Info).env().init().unwrap();
