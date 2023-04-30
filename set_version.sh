4ea8bc2d1584db761f0e9062639da000472f700f
Mattia Di Eleuterio <madiele92@gmail.com>
2023-04-30 20:49:44 +0200

    add version to homepage

--- set_version.sh
+++ set_version.sh
@@ -8,4 +8,7 @@ else
 fi
 
 sed "s/^version = .*$/version = \"$VERSION\"/" Cargo.toml > Cargo.toml.tmp
+VERSION_HTML="<small class=\"text-muted\">$VERSION<\/small>"
+sed "s/<\!-- ###VERSION### -->/$VERSION_HTML/" templates/index.html > templates/index.html.tmp
 mv Cargo.toml.tmp Cargo.toml
+mv templates/index.html.tmp templates/index.html
