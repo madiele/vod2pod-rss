b2a515c69dc5a31cb44c50ba5a541710a57f734e
Mattia Di Eleuterio <MDiEleuterio@qubicaamf.com>
2023-11-03 22:10:58 +0100

    implement generic provider

--- src/provider/generic.rs
+++ src/provider/generic.rs
@@ -5,23 +5,29 @@ use reqwest::Url;
 
 use crate::configs::{conf, Conf, ConfName};
 
-use super::MediaProvider;
+use super::{MediaProvider, MediaProviderV2};
 
 pub(super) struct GenericProvider {
-    url: Url
+    url: Url,
 }
 
 #[async_trait]
 impl MediaProvider for GenericProvider {
-    fn new(url: &Url) -> Self { GenericProvider { url: url.clone() } }
+    fn new(url: &Url) -> Self {
+        GenericProvider { url: url.clone() }
+    }
 
-    async fn get_item_duration(&self, _url: &Url) -> eyre::Result<Option<u64>> { Ok(None) }
+    async fn get_item_duration(&self, _url: &Url) -> eyre::Result<Option<u64>> {
+        Ok(None)
+    }
 
     async fn get_stream_url(&self, media_url: &Url) -> eyre::Result<Url> {
         Ok(media_url.to_owned())
     }
 
-    async fn filter_item(&self, _rss_item: &Entry) -> bool { false }
+    async fn filter_item(&self, _rss_item: &Entry) -> bool {
+        false
+    }
 
     fn media_url_regexes(&self) -> Vec<Regex> {
         let generic_whitelist = get_generic_whitelist();
@@ -29,12 +35,44 @@ impl MediaProvider for GenericProvider {
         #[cfg(not(test))]
         return generic_whitelist;
         #[cfg(test)] //this will allow test to use localhost ad still work
-        return [generic_whitelist, vec!(regex::Regex::new(r"^http://127\.0\.0\.1:9872").unwrap())].concat();
+        return [
+            generic_whitelist,
+            vec![regex::Regex::new(r"^http://127\.0\.0\.1:9872").unwrap()],
+        ]
+        .concat();
     }
 
-    fn domain_whitelist_regexes(&self) -> Vec<Regex> { get_generic_whitelist() }
+    fn domain_whitelist_regexes(&self) -> Vec<Regex> {
+        get_generic_whitelist()
+    }
 
-    async fn feed_url(&self) -> eyre::Result<Url> { Ok(self.url.clone()) }
+    async fn feed_url(&self) -> eyre::Result<Url> {
+        Ok(self.url.clone())
+    }
+}
+
+struct GenericProviderV2 {}
+
+#[async_trait]
+impl MediaProviderV2 for GenericProviderV2 {
+    async fn generate_rss_feed(&self, channel_url: Url) -> eyre::Result<String> {
+        Ok(reqwest::get(channel_url).await?.text().await?)
+    }
+
+    async fn get_stream_url(&self, media_url: &Url) -> eyre::Result<Url> {
+        Ok(media_url.to_owned())
+    }
+
+    fn domain_whitelist_regexes(&self) -> Vec<Regex> {
+        get_generic_whitelist()
+    }
+
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        GenericProviderV2 {}
+    }
 }
 
 fn get_generic_whitelist() -> Vec<Regex> {
@@ -43,7 +81,8 @@ fn get_generic_whitelist() -> Vec<Regex> {
 
     let mut regexes: Vec<Regex> = Vec::with_capacity(patterns.len() + 1);
     for pattern in patterns {
-        regexes.push(Regex::new(&pattern.to_string().replace(".", "\\.").replace("*", ".+")).unwrap())
+        regexes
+            .push(Regex::new(&pattern.to_string().replace(".", "\\.").replace("*", ".+")).unwrap())
     }
 
     let match_extensions = regex::Regex::new("^(https?://)?.+\\.(mp3|mp4|wav|avi|mov|flv|wmv|mkv|aac|ogg|webm|3gp|3g2|asf|m4a|mpg|mpeg|ts|m3u|m3u8|pls)$").unwrap();
