5a46c858aa58a5507270c7506c8a3677f3902c65
Mattia Di Eleuterio <MDiEleuterio@qubicaamf.com>
2023-11-03 22:08:49 +0100

    implement peertube

--- src/provider/peertube.rs
+++ src/provider/peertube.rs
@@ -6,10 +6,10 @@ use serde::Deserialize;
 
 use crate::configs::{conf, Conf, ConfName};
 
-use super::MediaProvider;
+use super::{MediaProvider, MediaProviderV2};
 
 pub(super) struct PeertubeProvider {
-    url: Url
+    url: Url,
 }
 
 #[allow(non_snake_case)]
@@ -27,8 +27,6 @@ struct StreamingPlaylist {
 
 #[async_trait]
 impl MediaProvider for PeertubeProvider {
-
-
     async fn get_item_duration(&self, media_url: &Url) -> eyre::Result<Option<u64>> {
         let video_url = find_api_url(media_url).await?;
 
@@ -55,7 +53,13 @@ impl MediaProvider for PeertubeProvider {
         let hosts = get_peertube_hosts();
         let mut regexes: Vec<Regex> = Vec::with_capacity(hosts.len());
         for host in hosts {
-            regexes.push(Regex::new(&(host.to_string().replace(".", "\\.").replace("*", ".+") + "/static/streaming-playlists/.*")).unwrap())
+            regexes.push(
+                Regex::new(
+                    &(host.to_string().replace(".", "\\.").replace("*", ".+")
+                        + "/static/streaming-playlists/.*"),
+                )
+                .unwrap(),
+            )
         }
 
         return regexes;
@@ -69,13 +73,52 @@ impl MediaProvider for PeertubeProvider {
         let hosts = get_peertube_hosts();
         let mut regexes: Vec<Regex> = Vec::with_capacity(hosts.len());
         for host in hosts {
-            regexes.push(Regex::new(&host.to_string().replace(".", "\\.").replace("*", ".+")).unwrap())
+            regexes
+                .push(Regex::new(&host.to_string().replace(".", "\\.").replace("*", ".+")).unwrap())
         }
 
         return regexes;
     }
 
-    async fn feed_url(&self) -> eyre::Result<Url> { Ok(self.url.clone()) }
+    async fn feed_url(&self) -> eyre::Result<Url> {
+        Ok(self.url.clone())
+    }
+}
+
+struct PeerTubeProviderV2 {}
+
+#[async_trait]
+impl MediaProviderV2 for PeerTubeProviderV2 {
+    async fn generate_rss_feed(&self, channel_url: Url) -> eyre::Result<String> {
+        Ok(reqwest::get(channel_url).await?.text().await?)
+    }
+
+    async fn get_stream_url(&self, media_url: &Url) -> eyre::Result<Url> {
+        let video_url = find_api_url(media_url).await?;
+
+        let response = reqwest::get(video_url).await?;
+        let video: Video = response.json().await?;
+
+        Ok(video.streamingPlaylists[0].playlistUrl.clone())
+    }
+
+    fn domain_whitelist_regexes(&self) -> Vec<Regex> {
+        let hosts = get_peertube_hosts();
+        let mut regexes: Vec<Regex> = Vec::with_capacity(hosts.len());
+        for host in hosts {
+            regexes
+                .push(Regex::new(&host.to_string().replace(".", "\\.").replace("*", ".+")).unwrap())
+        }
+
+        return regexes;
+    }
+
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        PeerTubeProviderV2 {}
+    }
 }
 
 async fn find_api_url(media_url: &Url) -> eyre::Result<Url> {
@@ -86,20 +129,28 @@ async fn find_api_url(media_url: &Url) -> eyre::Result<Url> {
         .unwrap()
         .find_map(|x| uuid::Uuid::parse_str(x).ok());
 
-    let uuid = foud_uuid.ok_or_else(|| {
-        eyre::eyre!("could not find uuid in: {:?}", video_url.to_string())
-    })?;
+    let uuid = foud_uuid
+        .ok_or_else(|| eyre::eyre!("could not find uuid in: {:?}", video_url.to_string()))?;
 
-    video_url.path_segments_mut().unwrap()
+    video_url
+        .path_segments_mut()
+        .unwrap()
         .clear()
-        .push("api").push("v1").push("videos").push(&uuid.to_string());
+        .push("api")
+        .push("v1")
+        .push("videos")
+        .push(&uuid.to_string());
 
     Ok(video_url)
 }
 
 fn get_peertube_hosts() -> Vec<String> {
     let binding = conf().get(ConfName::PeerTubeValidHosts).unwrap();
-    let patterns: Vec<String> = binding.split(",").filter(|e| e.trim().len() > 0).map(|x| x.to_string()).collect();
+    let patterns: Vec<String> = binding
+        .split(",")
+        .filter(|e| e.trim().len() > 0)
+        .map(|x| x.to_string())
+        .collect();
     return patterns;
 }
 
